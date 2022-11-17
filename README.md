b6b7fcfbdf48109635f954ecb4ce9588a06afe68
Daniel Harnack <daniel.harnack@dfki.de>
2022-11-17 15:51:46 +0100

    running rl code

--- README.md
+++ README.md
@@ -9,3 +9,33 @@ This project offers an open-source and low-cost kit to test control algorithms f
 it implements a **brachiation robot** platform called **AcroMonk** which can brachiate robustly over a horizontally laid ladder bar. AcroMonk is the simplest possible underactuated brachiator built using one quasi-direct drive actuator (QDD) and passive grippers. This is the first brachiator with unactuated grippers that can perform more than two brachiation maneuvers. This project offers different control methods for trajectory stabilization which can be studied using the kit. Additionally, it provides a list of components, discusses best practices for implementation, and presents results from experiments with the simulator and the real system. This repository describes the hardware (CAD, Bill Of Materials (BOM) etc.) required to build the physical system and provides the software (URDF models, simulation and controllers) to control it.
 
 ## Reinforcement Learning
+This installation instruction assumes Ubuntu 20.04. 
+We recommend using a dedicated python virtual environment, e.g. using 
+
+    python3 -m venv path/to/your/new/venv
+
+The training and simulation environment uses Mujoco 2.1, which has to be installed first. In order to easily 
+find the package, you can add 
+
+    export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/path/to/mujoco/folder/bin
+
+to your .bashrc. The path is ~/.mujoco/mujoco210/bin by default (see https://github.com/openai/mujoco-py 
+for more instructions). 
+
+The requirements can be installed from the file located in 
+reinforcement_learning/requirements.txt (e.g. 
+<code>pip install -r requirements.txt</code>)
+
+
+Hint: if you face an 
+
+    GLEW intialization error: Missing GL version
+
+adding 
+
+    export LD_PRELOAD=$LD_PRELOAD:/usr/lib/x86_64-linux-gnu/libGLEW.so
+
+to .bashrc can help.
+
+To simulate the trained rl controller, move to the reinforcement_learning/scripts 
+folder and run replay_rl_model.py.
--- reinforcement_learning/scripts/pytorch_to_numpy_controller.py
+++ reinforcement_learning/scripts/pytorch_to_numpy_controller.py
@@ -1,14 +1,16 @@
-# Here check whether the pytorch model and the numpy version give the same torque commands
+# Simple script to check whether pytorch and numpy policy give the same torque output before deployment
 
+# system imports
 import os
-from utils.paths_handling import project_root_dir
-from utils.make_rl_model import load_rl_model
 import numpy as np
 import torch
-import time
 import matplotlib.pyplot as plt
 from matplotlib import cm
-
+# local imports
+import sys
+sys.path.append('../')
+from utils.paths_handling import project_root_dir
+from utils.make_rl_model import load_rl_model
 
 
 def predict_numpy(obs, weights, biases):
@@ -20,7 +22,7 @@ def predict_numpy(obs, weights, biases):
 
     return activation.T
 
-rl_model_path = os.path.join(project_root_dir, 'data', 'trained_agents', '2022_9_7_17_55_16')
+rl_model_path = os.path.join(project_root_dir, 'data', 'trained_controllers')
 rl_checkpoint = 'best'
 
 rl_model, parameters, env = load_rl_model(rl_model_path, rl_checkpoint)
@@ -42,13 +44,6 @@ for weight_key, bias_key in zip(weight_keys, bias_keys):
     policy_weights_np.append(model_pars['policy'][weight_key].numpy())
     policy_biases_np.append(model_pars['policy'][bias_key].numpy())
 
-
-
-np.save('weights', policy_weights_np, allow_pickle=True)
-np.save('biases', policy_biases_np, allow_pickle=True)
-loaded_weights = np.load('weights.npy', allow_pickle=True)
-loaded_biases = np.load('biases.npy', allow_pickle=True)
-
 np_predictions = []
 time_to_predict = []
 loaded_np_predictions = []
@@ -59,44 +54,10 @@ rand_obs = (2 * np.random.rand(1000, 4) - 1) * np.array([np.pi, np.pi, 4.0, 4.0]
 rl_model_predict = rl_model.policy.predict(torch.tensor(rand_obs), deterministic=True)[0]
 np_predict = predict_numpy(rand_obs, policy_weights_np, policy_biases_np)
 
-# plt.figure('Compare pytorch and numpy torque, sanity check')
-# plt.plot(np.squeeze(rl_model_predict), np.squeeze(rl_model_predict), 'r-', zorder=0)
-# plt.scatter(np.squeeze(rl_model_predict), np.squeeze(np_predict), 2, zorder=1)
-# plt.xlabel('pytorch model torque')
-# plt.ylabel('numpy model torque')
-# plt.show()
-# X, Y = np.meshgrid(rand_obs[:, 2], rand_obs[:, 3])
-# plt.figure('What is the controller actually doing?')
-# plt.pcolor(X, Y, rl_model_predict)
-# plt.show()
-
-X, Y = np.meshgrid(rand_obs[:, 2], rand_obs[:, 3])
-fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
-surf = ax.plot_surface(X, Y, rl_model_predict, cmap=cm.coolwarm,
-                       linewidth=0, antialiased=False)
+plt.figure('Compare pytorch and numpy torque, sanity check')
+plt.plot(np.squeeze(rl_model_predict), np.squeeze(rl_model_predict), 'r-', zorder=0)
+plt.scatter(np.squeeze(rl_model_predict), np.squeeze(np_predict), 2, zorder=1)
+plt.xlabel('pytorch model torque')
+plt.ylabel('numpy model torque')
 plt.show()
-
-a = None
-
-# for k in range(10):
-#     rand_obs = np.random.rand(4)
-#     time_pre_predict = time.time()
-#     np_predict = predict_numpy(rand_obs, policy_weights_np, policy_biases_np)
-#     time_post_predict = time.time()
-#     time_to_predict.append(time_post_predict - time_pre_predict)
-#     loaded_np_predict = predict_numpy(rand_obs, loaded_weights, loaded_biases)
-#     rl_model_predict = rl_model.policy.predict(torch.tensor(rand_obs), deterministic=True)[0]
-#
-#     np_predictions.append(np_predict[0])
-#     loaded_np_predictions.append(loaded_np_predict[0])
-#     rl_model_predictions.append(rl_model_predict[0])
-#
-# print(f'Predictions stable_baselines:{rl_model_predictions}')
-# print(f'Predictions numpy:{np_predictions}')
-# print(f'Predictions numpy loaded:{loaded_np_predictions}')
-# print(f'Predictions time numpy:{time_to_predict}')
-
-a = None
-
-
-
+X, Y = np.meshgrid(rand_obs[:, 2], rand_obs[:, 3])
