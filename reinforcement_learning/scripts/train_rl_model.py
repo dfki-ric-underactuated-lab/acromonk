07cb8254f3a3c824073894c874cfe8a9bfdafac7
dharnack <dharnack@dfki.de>
2022-11-18 11:19:38 +0100

    first full version of  rl readme

--- reinforcement_learning/scripts/train_rl_model.py
+++ reinforcement_learning/scripts/train_rl_model.py
@@ -6,7 +6,7 @@ from training.trainer import acromonk_training
 # get all parameters
 k_trains = 1
 for k in range(k_trains):
-    with open(os.path.join(project_root_dir, 'parameters', 'parameters.json')) as config:
+    with open(os.path.join(project_root_dir, 'training', 'parameters.json')) as config:
         parameters = json.load(config)
 
     # do the training and saving
